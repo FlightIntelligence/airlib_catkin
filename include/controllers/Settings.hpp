5f9be7e9b8e1bdbf03d0addd0c9f171cc8b78db4
Jorik De Waen <jorik.de.waen@gmail.com>
2017-10-25 17:19:16 +0200

    updated code from AirSim repo

--- include/controllers/Settings.hpp
+++ include/controllers/Settings.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 #ifndef msr_airlib_settings_hpp
